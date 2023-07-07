a856870967f4a2e7295e58a39abaa6d02af29779
Lee Rogers <edwinleerogers@gmail.com>
2023-07-07 13:16:01 -0700

    fill in Repo component, add back btn

--- app/code/repos/[name]/page.tsx
+++ app/code/repos/[name]/page.tsx
@@ -1,3 +1,6 @@
+import Link from 'next/link'
+import Repo from "@/app/components/Repo"
+
 interface PageProps {
   params: { name: string }
 }
@@ -5,8 +8,8 @@ interface PageProps {
 const RepoPage = ({ params: { name } }: PageProps) => {
   return (
     <div className="card">
-      <h2>{name}</h2>
-      <p>Repo Details</p>
+      <Link href="/code/repos" className='btn btn-back'>Go Back to Repositories</Link>
+      <Repo name={name} />
     </div>
   )
 }
--- app/components/Repo.tsx
+++ app/components/Repo.tsx
@@ -1,3 +1,6 @@
+import Link from "next/link"
+import { FaStar, FaCodeBranch, FaEye } from "react-icons/fa"
+
 interface RepoProps {
   name: string
 }
@@ -11,7 +14,24 @@ async function fetchRepo(name: string) {
 const Repo = async ({ name }: RepoProps) => {
   const repo = await fetchRepo(name)
   return (
-    <div>Repo</div>
+    <>
+      <h2>{repo.name}</h2>
+      <p>{repo.description}</p>
+      <div className="card-stats">
+        <div className="card-stat">
+          <FaStar />
+          <span>{repo.stargazers_count}</span>
+        </div>
+        <div className="card-stat">
+          <FaCodeBranch />
+          <span>{repo.forks_count}</span>
+        </div>
+        <div className="card-stat">
+          <FaEye />
+          <span>{repo.watchers_count}</span>
+        </div>
+      </div>
+    </>
   );
 }
  
